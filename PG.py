909ae922af222f44f8517c93375dd72e89db9e89
Juwan B <juwan2003@icloud.com>
2024-02-20 13:14:47 -0500

    Update

--- PG.py
+++ PG.py
@@ -1,3 +1,14 @@
-def hello(first,middle,last):
-    print(f"Hello {first} {middle} {last}")
-hello("Bro","Dude","Code")
+import time
+class Vehicle:
+    def __init__(self,color,brand,model):
+        self.color = color
+        self.brand = brand
+        self.model = model
+
+    def drifting(self):
+        time.sleep(2)
+        print(f"The all new {self.color} {self.brand} {self.model} is being driving across the highway!")
+
+car = Vehicle("Red","Dodge","Charger")
+car.driving()
+
