76debfeedc4490325d853dfab4bdf10f0c06870d
Juwan B <juwan2003@icloud.com>
2024-04-13 17:53:42 -0400

    Updated code

--- PG.py
+++ PG.py
@@ -1,13 +1,5 @@
-import time
-class Vehicle:
-    def __init__(self,color,brand,model):
-        self.color = color
-        self.brand = brand
-        self.model = model
-
-    def drifting(self):
-        time.sleep(2)
-        print(f"The all new {self.color} {self.brand} {self.model} is being driving across the highway!")
-
-car = Vehicle("Red","Dodge","Charger")
-car.drifting()
+def evennumbers():
+    for i in range(2,11):
+        if i%2 == 0:
+            print(i)
+evennumbers()
